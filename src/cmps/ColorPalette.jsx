661abd1b35b7215ea02fbf0cdda2527b7604b361
Yuval Shmukler <yuval.shmukler@gmail.com>
2024-12-01 11:36:44 +0200

    added palette colors and fix cards layout

--- src/cmps/ColorPalette.jsx
+++ src/cmps/ColorPalette.jsx
@@ -1,22 +1,21 @@
 import { useSelector } from 'react-redux'
 import { useState, useRef, useEffect } from 'react'
-
 export function ColorPalette({ onColorClick, isOpen, toggleModal, colorButtonRef }) {
     const modalRef = useRef(null);
 
     const colors = [
-        '#FF6F61',
-        '#6B5B95',
-        '#88B04B',
-        '#F7CAC9',
-        '#92A8D1',
-        '#F5D76E',
-        '#E94B3C',
-        '#4AA3DF',
-        '#B565A7',
-        '#5B8C5A',
-        '#FFB347',
-        '#A2C5AC',
+        '#faafa8',
+        '#f39f76',
+        '#fff8b8',
+        '#e2f6d',
+        '#b4ddd3',
+        '#d4e4ed',
+        '#d4e4e',
+        '#aeccdc',
+        '#d3bfdb',
+        '#f6e2dd',
+        '#e9e3d4',
+        '#efeff1',
     ];
     const imgs = [
         '#FF6F61',
